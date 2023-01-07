9c6d9b7fbc15d5c8d3c32b18b639ef3b1651062c
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-01-07 23:42:50 +0545

    add delete button to output

--- src/component/Output/ToDoItem.jsx
+++ src/component/Output/ToDoItem.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const ToDoItem = ({ data }) => {
-	const styles={
-		cursor:"pointer"
-	}
-	return(
-		<ul>
-			<li style={styles}>{data}</li>
-		</ul>
-	)
+  const styles = {
+    cursor: "pointer",
+  };
+  return (
+    <ul style={{ display: "flex", gap: 20 }}>
+      <li style={styles}>{data}</li>
+      <button>Delete</button>
+    </ul>
+  );
 };
 
 export default ToDoItem;
